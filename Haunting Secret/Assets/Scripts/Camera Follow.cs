8fb55120eaadd8a8bff0bcbb43089e6259047dc1
murilomatiaspaiano <murilo.310223@alunosatc.edu.br>
2024-11-20 00:11:13 -0300

    Camera e outras atualizações

---
+++ Haunting Secret/Assets/Scripts/Camera Follow.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float timeLerp;
+    public Transform player;
+    private void FixedUpdate()
+    {
+        Vector3 newPosition = player.position + new Vector3(0, 0, -10);
+        newPosition.y = 0.1f;
+        newPosition = Vector3.Lerp(transform.position, newPosition, timeLerp);
+        transform.position = newPosition;
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x,0.1f,7.97f),transform.position.y, transform.position.z);
+    }
+}
