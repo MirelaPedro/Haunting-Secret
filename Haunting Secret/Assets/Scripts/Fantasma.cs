1f49c69217841afbea4376305aa08edc3d7747ba
murilomatiaspaiano <murilo.310223@alunosatc.edu.br>
2024-11-22 08:59:29 -0300

    qe

--- Haunting Secret/Assets/Scripts/Fantasma.cs
+++ Haunting Secret/Assets/Scripts/Fantasma.cs
@@ -21,7 +21,6 @@ public class Fantasma : MonoBehaviour
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
         ground = Physics2D.Linecast(groundCheck.position, transform.position, groundLayer);
-        Debug.Log(ground);
 
         if(ground == false)
         {
